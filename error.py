e32a2d480202d12e57cc0b2c59a423fa3f795714
Jakub Piskiewicz <jakub@piskiewicz.org>
2024-11-10 22:15:59 +0100

    STUB: error functions

---
+++ error.py
@@ -0,0 +1,17 @@
+"""
+Functions for computing different types of error rates for the segmentation problem.
+"""
+
+from tinygrad.tensor import Tensor
+
+
+def warping_error(t1: Tensor, t2: Tensor) -> float:
+  pass
+
+
+def  rand_error(t1: Tensor, t2: Tensor) -> float:
+  pass
+
+
+def pixel_error(t1: Tensor, t2: Tensor) -> float:
+  pass
