867f8e8fa890178e4671a4429792c3f313564310
Jakub Piskiewicz <jakub@piskiewicz.org>
2024-11-11 18:01:22 +0100

    Add pixel error

--- error.py
+++ error.py
@@ -6,12 +6,20 @@ from tinygrad.tensor import Tensor
 
 
 def warping_error(t1: Tensor, t2: Tensor) -> float:
-  pass
+  """
+  Reference:
+  https://github.com/fiji/Trainable_Segmentation/blob/41c7083382816b8d40deffbb06c9c3eace0b1e55/src/main/java/trainableSegmentation/metrics/WarpingError.java
+  """
+  return -1
 
 
 def  rand_error(t1: Tensor, t2: Tensor) -> float:
-  pass
+  return -1
 
 
 def pixel_error(t1: Tensor, t2: Tensor) -> float:
-  pass
+  """
+  Pixel error is just the Euclidean distance between two binary-valued
+  tensors.
+  """
+  return (t1 - t2).square().mean().numpy().item()
