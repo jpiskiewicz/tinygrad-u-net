7ef830e785da70d16cb6f2a0b4cbf21b4e7a22d7
Jakub Piskiewicz <jakub@piskiewicz.org>
2025-11-16 16:40:37 +0100

    Prepare inference script

--- dataset.py
+++ dataset.py
@@ -11,11 +11,11 @@ from tinygrad.dtype import dtypes
 from helpers import plot_slice
 import numpy as np
 
-# For now the idea is to train the net on the 120th slice of each saggital view of the MRI scan. 
+# For now the idea is to train the net on the 120th slice of each saggital view of the MRI scan.
 # I think that if we figure out how to train the net on such a simple scenario, then figuring out
 # how to train it for other (more complex use-cases) will be easier.
- 
-SLICE = 120 
+
+SLICE = 120
 INPUT_SIZE = 240
 
 
@@ -26,28 +26,30 @@ def choose_files(pattern):
   train = directories[:idx]
   val = directories[idx:]
   with open("validation_files.json", "w") as f: json.dump(val, f)
+  with open("training_files.json", "w") as f: json.dump(train, f)
   return train, val
+
+
+def load_slice(path: str, mask: bool) -> Tensor:
+  s = Tensor(nib.load(path).get_fdata(dtype=np.float16)[SLICE]).flip(1).transpose().clamp(0, 1 if mask else None)
+  not_even = [s.shape[i] % 2 for i in range(2)]
+  [y_pad, x_pad] = [(INPUT_SIZE - s.shape[i]) // 2 for i in range(2)]
+  s = s.pad((x_pad, x_pad + (1 if not_even[1] else 0), y_pad, y_pad + (1 if not_even[0] else 0))).expand(1, 1, -1, -1)
+  if mask: return s
+  s = s.cast(dtypes.float32)
+  return ((s - s.mean()) / (s.std() + 1e-8)).cast(dtypes.float16) # z-score normalization
 
 
 class Dataset:
   def __init__(self, directories: list[str]): self.images, self.labels = self.get_slices(self.get_files(directories))
-    
+
   def get_files(self, directories: list[str]) -> list[list[str]]: return [[path.join(dirname, Path(dirname).name + "_" + x + ".nii") for dirname in directories] for x in ["t1ce", "seg"]]
-  
-  def get_slices(self, data: list[list[str]]) -> list[Tensor]: return [self.combine([self.load_slice(x, i == 1) for x in fileset]) for i, fileset in enumerate(data)]
-  
-  def load_slice(self, path: str, mask: bool) -> Tensor:
-    s = Tensor(nib.load(path).get_fdata(dtype=np.float16)[SLICE]).flip(1).transpose().clamp(0, 1 if mask else None)
-    not_even = [s.shape[i] % 2 for i in range(2)]
-    [y_pad, x_pad] = [(INPUT_SIZE - s.shape[i]) // 2 for i in range(2)]
-    s = s.pad((x_pad, x_pad + (1 if not_even[1] else 0), y_pad, y_pad + (1 if not_even[0] else 0))).expand(1, 1, -1, -1)
-    if mask: return s
-    s = s.cast(dtypes.float32)
-    return ((s - s.mean()) / (s.std() + 1e-8)).cast(dtypes.float16) # z-score normalization
-  
+
+  def get_slices(self, data: list[list[str]]) -> list[Tensor]: return [self.combine([load_slice(x, i == 1) for x in fileset]) for i, fileset in enumerate(data)]
+
   def combine(self, slices: list[Tensor]) -> Tensor: return slices[0].stack(*slices[1:])
-  
-  
+
+
 if __name__ == "__main__":
    train, val = choose_files("dataset/MICCAI_BraTS_2019_Data_Training/*GG/*")
    dataset = Dataset(val)
